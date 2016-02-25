d044d9069ca4a5df99ffd598314227eaf5979201
Piotr Flasza <piotr.flasza@enp.pl>
2016-02-25 16:13:20 +0100

    test kopiowanie koleiki do listy zadan

--- src/Queue.php
+++ src/Queue.php
@@ -65,64 +65,6 @@ class Queue
 	 * @param int $size
 	 * @return TaskList
 	 */
-//	public function getTaskList($size = 100)
-//	{
-//
-//		/**
-//		 * copy part of queue to other redis list
-//		 */
-//		$taskListsKey = $this->getName() . ':task_lists';
-//		$queueName = $this->getName();
-//		
-//		$script =
-//			'
-//				taskListUniqueName = KEYS[1]
-//				taskListsKey = KEYS[2]
-//				size = KEYS[3]
-//				queueName = KEYS[4]
-//				-- check if key is unique
-//				is_unique = redis.call("sismember",taskListsKey, taskListUniqueName)
-//				if is_unique != 0 then return 0 end
-//				
-//				messages = redis.call("lrange",queueName,0,size)
-//				redis.call("sadd",taskListsKey,taskListUniqueName)
-//				redis.call("lpush",taskListUniqueName,messages)
-//				redis.call("ltrim",queueName,size,-1)
-//				return 1
-//			';
-//		
-//		
-////		$taskListName = $this->client->transaction(function ($tx) use ($taskListsKey, $queueName, $size) {
-////
-//////			$messages = $tx->lrange($queueName, 0, $size);
-//////			if (count($messages) == 0) {
-//////				throw new Exception("Queue '$queueName' is empty or not exists at all !");
-//////			}
-////			
-////			/**
-////			 * create uniqu name
-////			 */
-////			$taskListUniqueName = md5(microtime() . mt_rand());
-//////			$isUnique = $tx->sismember($taskListsKey, $taskListUniqueName);
-//////			if ($isUnique != 0) {
-//////				throw new Exception("Task list '$taskListUniqueName' exists in list '$taskListsKey', very rare problem, try run process again !");
-//////			}
-////			$tx->sadd($taskListsKey, $taskListUniqueName);
-////
-////			
-////			//$tx->lpush($taskListUniqueName, $messages);
-////			$tx->ltrim($queueName, 0, $size);
-////			
-////		});
-//
-//		var_dump($taskListName);
-//		exit;
-//		
-//		return new TaskList();
-//	}
-
-
-
 	public function getTaskList($size = 100)
 	{
 		/**
@@ -130,18 +72,20 @@ class Queue
 		 */
 		$taskListUniqueName = $this->getName().':'.md5(microtime() . mt_rand());
 		$queueName = $this->getName();
-
+		$timestamp = time();
+		
 		$script = '
 				local taskListUniqueName = KEYS[1]
 				local queueName = KEYS[2]
 				local size = KEYS[3]
+				local timestamp = KEYS[4]
 				-- check if key is unique
 				local is_unique = redis.call("exists", taskListUniqueName)
 				if is_unique == 1 then return 0 end
 				
 				local messages = redis.call("lrange",queueName,0,size)
 				for key,message in pairs(messages) do
-					redis.call("lpush",taskListUniqueName,message)
+					redis.call("hset",taskListUniqueName,message,timestamp)
 				end
 				redis.call("ltrim",queueName,size,-1)
 				return 1
@@ -150,10 +94,15 @@ class Queue
 		/**
 		 * 1. check llen of queue
 		 */
+		if ($this->client->llen($queueName) == 0) {
+			throw new Exception("Queue '$queueName' is empty or not exists !");
+		}
+		
+		
 		/**
 		 * 2. create task list
 		 */
-		$res = $this->client->eval($script, 3, $taskListUniqueName, $queueName, $size);
+		$res = $this->client->eval($script, 4, $taskListUniqueName, $queueName, $size, $timestamp);
 		if ($res == 0) {
 			throw new Exception("Task list '$taskListUniqueName' exists in list '$taskListsKey', very rare problem, try run process again !");
 		} elseif ($res == 1) {
